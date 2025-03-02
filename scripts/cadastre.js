29e6cf1739aa798c56f1ea9c76b0e7df9a9683f2
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2025-03-01 21:36:57 -0300

    organizei o código

--- scripts/cadastre.js
+++ scripts/cadastre.js
@@ -1,3 +1,4 @@
+// HTML INPUTS
 const $inputStudentName = document.querySelector("#nameStudentCadastre");
 const $inputDayBirth = document.querySelector("#dayBirthCadastre");
 const $inputMonthBirth = document.querySelector("#monthSelect");
@@ -13,18 +14,17 @@ const $inputResponsibleEmail = document.querySelector(
 );
 const $inputResponsibleTel = document.querySelector("#telResponsibleCadastre");
 const form = document.querySelector("#formCadastre");
-let isValidForm = false;
+const textInputs = document.querySelectorAll(".stringTypeInput");
+const numberInputs = document.querySelectorAll(".numberTypeInput");
+let isValidForm = false; // CONTROL ERRORS
 
-form.onsubmit = (event) => {
-  event.preventDefault();
-  checkForm();
-};
-
+// CHECK INPUTS FORM
 let checkForm = () => {
   checkRadiosInput();
   checkTextInputs();
   if (isValidForm) {
-    alert("Form enviado");
+    alert("Form enviado"); // FOR VIEW
+    // OBJECT WITH INPUTS VALUES
     const cadastro = {
       name: $inputStudentName.value,
       dateBirth: {
@@ -41,38 +41,67 @@ let checkForm = () => {
       ageGroup: document.querySelector('input[name="ageGroup"]:checked').value,
       acceptTerms: document.querySelector('input[name="terms"]:checked').value,
     };
-    console.log(cadastro)
   } else {
     newError("Por favor, prencha todos os campos obrigatórios");
   }
 };
 
+// CHECK RADIO INPUTS WAS CHECKED
 let checkRadiosInput = () => {
   const $ageGroupRadio = document.querySelector(
     'input[name="ageGroup"]:checked'
   );
   const $termsRadio = document.querySelector('input[name="terms"]:checked');
-  if ($ageGroupRadio === null || $termsRadio === null) {
-    newError("Por favor, selecione ao menos uma opção.");
-  } else {
+  if ($ageGroupRadio !== null || $termsRadio !== null) {
     return (isValidForm = true);
   }
 };
 
+// CREATE A BOOTSTRAP TOAST FOR INVALID INPUTS
 let newError = (message) => {
   const errorToast = new bootstrap.Toast(document.getElementById("liveToast"));
   document.querySelector(".toast-body").textContent = message;
   errorToast.show();
 };
 
+// CHECK ALL INPUTS IS NOT EMPTY
 let checkTextInputs = () => {
-    const inputs = [$inputStudentName, $inputNameMother, $inputNameFather]
-    isValidForm = inputs.every((input) => input.value !== "")
-    inputs.map((input) => {
-        onlyLetters()
-    })
-}
+  const inputs = document.querySelectorAll("input");
+  isValidForm = Array.from(inputs).every((input) => input.value !== "");
+};
 
-let onlyLetters = (input) => {
-    return input.replace(/[^A-Za-zÀ-ÿ\s]/gu, "")
-}
+// REGEX FOR ALL INPUTS TYPEOF TEXT
+textInputs.forEach((input) => {
+  input.addEventListener("input", () => {
+    input.value = onlyLetters(input.value);
+  });
+});
+
+// REGEX FOR ALL INPUTS TYPEOF NUMBER
+numberInputs.forEach((input) => {
+  input.addEventListener("input", () => {
+    input.value = onlyNumbers(input.value);
+  });
+});
+
+// REGEX FOR TEXT INPUTS
+let onlyLetters = (value) => {
+  return value.replace(/[^A-Za-zÀ-ÿ\s]/gu, "");
+};
+
+// REGEX FOR NUMBER INPUTS
+let onlyNumbers = (value) => {
+  return value.replace(/\D/g, "");
+};
+
+// REGEX FOR EMAIL INPUT
+let validateEmail = (email) => {
+  const regex = /\S+@\S+\.\S+/;
+  return regex.test(email)
+};
+
+// SUBMIT FORM
+form.onsubmit = (event) => {
+  event.preventDefault();
+  checkForm();
+};
